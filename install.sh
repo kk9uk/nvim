a1d4f0996c3942d62e7a93327c889fc3f7bd6860
kk9uk <122882463+kk9uk@users.noreply.github.com>
2024-10-13 00:23:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -eEuo pipefail
 trap 'echo "[ERROR]: Line $LINENO of $0"' ERR
 
 # Put AppImage at .local/bin
+mkdir -p $HOME/.local/bin
 sudo apt install curl fuse -y
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod 755 nvim.appimage
