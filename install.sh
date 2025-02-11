a4ae9e09c884a8d9b2a8ea442b808b044c199be1
kk9uk <122882463+kk9uk@users.noreply.github.com>
2025-02-12 07:57:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ trap 'echo "[ERROR]: Line $LINENO of $0"' ERR
 # Put AppImage at .local/bin
 mkdir -p $HOME/.local/bin
 sudo apt install curl fuse -y
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod 755 nvim.appimage
-mv nvim.appimage $HOME/.local/bin/nvim
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
+chmod 755 nvim-linux-x86_64.appimage
+mv nvim-linux-x86_64.appimage $HOME/.local/bin/nvim
 
 # LSP dependencies
 sudo apt install python3.11-venv unzip ripgrep -y
